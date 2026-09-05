2343a5bdf8ed4831cc28f32ff586cd90e8391474
Divyansh <kackkerdivyansh1115@gmail.com>
2026-09-06 01:37:34 +0530

    Initial commit: Spring AI agent setup

---
+++ src/test/java/com/divyansh/aiagent/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.divyansh.aiagent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
